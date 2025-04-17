c57e3b0e1c5540ef213a6c079b402f0a9147c6b8
Robert Barrett <roborobbie123@gmail.com>
2025-04-17 15:56:26 -0400

    added a input components for add place implementation with validation

---
+++ src/places/components/Input.jsx
@@ -0,0 +1,88 @@
+import React, { useReducer, useEffect } from "react";
+import { validate } from "../../shared/validators";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default function Input({ ...props }) {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
+  const handleChange = (event) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const inputStyles =
+    "bg-gray-100 hover:bg-gray-200 border-gray-400 hover:border-gray-700 border mb-1 rounded-sm";
+
+  const handleTouch = () => {
+    dispatch({
+      type: "TOUCH",
+    });
+  };
+
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        className={inputStyles}
+        onChange={handleChange}
+        onBlur={handleTouch}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        className={inputStyles}
+        onChange={handleChange}
+        onBlur={handleTouch}
+        value={inputState.value}
+      />
+    );
+
+  const invalidStyle = "text-red-500 border-red-500";
+
+  return (
+    <div
+      className={`bg-white text-black flex flex-col gap-1 px-2 py-5 rounded-sm w-[20rem] md:w-[30rem] mt-2 ${
+        !inputState.isValid && inputState.isTouched && invalidStyle
+      }`}
+    >
+      <label className='font-semibold' htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+    </div>
+  );
+}
--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+import Input from "../components/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/validators";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default function NewPlace() {
-    return (
-        <div className='mt-10 flex justify-center text-white'>
-            Places works
-            </div>
-    );
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  return (
+    <div className="mx-auto mt-10 flex justify-center bg-white px-2 py-5 rounded-sm w-[20rem] md:w-[30rem] shadow-sm">
+      <form>
+        <Input
+          id="title"
+          type="text"
+          label="Title"
+          element="input"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title"
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          label="Description"
+          element="textarea"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)"
+          onInput={inputHandler}
+        />
+        <button
+          type="submit"
+          disabled={!formState.isValid}
+          className="border ml-2 py-1 px-5 text-center text-white bg-red-500 hover:bg-white hover:text-red-500 rounded-md shadow-lg disabled:bg-gray-600 disabled:text-gray-400"
+        >
+          ADD PLACE
+        </button>
+      </form>
+    </div>
+  );
 }
